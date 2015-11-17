75d2747507a2d13c1deccb9a610d114d06762f35
yiru <ah.yiru@gmail.com>
2015-11-17 18:39:13 +0800

    0.0.1'

--- admin/src/js/controllers/mytable.js
+++ admin/src/js/controllers/mytable.js
@@ -1,17 +1,41 @@
 // tab controller
+
 app.controller('mytable',  function($scope,$http) {
+	/*$scope.pagingOptions = {
+        pageSizes: [10, 20, 50],
+        pageSize: 10,
+        currentPage: 1
+    }; */
 	$scope.gridOptions={
-		/*enableSorting:true,
+		//rowHeight: 36,
+		//enableSorting:true,
+		//enableFiltering: false,
+
+		//showGridFooter: true,
+    	//enableGridMenu: true,
+
+    	enableHorizontalScrollbar:0,//uiGridConstants.scrollbars.NEVER,
+    	enableVerticalScrollbar:0,//uiGridConstants.scrollbars.NEVER,
+
+    	enablePaging: true,
+		paginationPageSizes: [10, 20, 50],
+    	paginationPageSize: 10,
+    	paginationCurrentPage: 1,
+		//pagingOptions: $scope.pagingOptions,
+
 		columnDefs:[
-			{name:'browser'},
-			{name:'engine'},
+			{name:'浏览器',field:'browser'},
+			{name:'engine',displayName:'引擎'},
 			{name:'grade'},
 			{name:'platform'},
-			{name:'version'}
+			{name:'version'},
+			{name:'edit',enableFiltering: false,width:100, enableCellEdit: true, cellTemplate: '<a href="#">编辑</a>'},
+			{name:'delete',enableFiltering: false,width:100, enableCellEdit: true, cellTemplate: '<a href="#">删除</a>'}
 		],
-		onRegisterApi:function(){
-			$scope.mygridApi=gridApi;
-		}*/
+
+		onRegisterApi:function(gridApi){
+			$scope.gridApi=gridApi;
+		}
 	};
 
   $http.get('api/datatable.json').then(function(data){
